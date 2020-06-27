88b9503ed6f08565f058630a275b793a9e48561f
Hayk <hayk.hayr3@gmail.com>
2020-06-28 02:48:21 +0400

    Add project files.

---
+++ MetanitLesson2.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MetanitLesson2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {j * i}");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
